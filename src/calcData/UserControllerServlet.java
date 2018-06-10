be0fbf8e9680aae49cc7fc9d9b80cafca17d1453
pepperoni666 <peretser08@gmail.com>
2018-06-10 23:58:31 +0200

    Password confitmation in registration page added

--- src/calcData/UserControllerServlet.java
+++ src/calcData/UserControllerServlet.java
@@ -17,43 +17,33 @@ public class UserControllerServlet extends HttpServlet {
         String email = request.getParameter("email");
         String password = request.getParameter("password");
         String conf_password = request.getParameter("conf_password");
-        UsersEntity user = new UsersEntity(email,password);
+        if(password.equals(conf_password) && !password.isEmpty()) {
+            UsersEntity user = new UsersEntity(email, password);
 
-        try{
-            UsersDao registerService = new UsersDao();
-            boolean result = registerService.register(user);
-            out.println("<html>");
-            out.println("<head>");
-            out.println("<title>Registration Successful</title>");
-            out.println("</head>");
-            out.println("<body>");
-            out.println("<center>");
-            if(result){
-                out.println("<h1>Thanks for Registering with us :</h1>");
-                out.println("To login with new UserId and Password<a href=login.jsp>Click here</a>");
-            }else{
-                out.println("<h1>Registration Failed</h1>");
-                out.println("To try again<a href=register.jsp>Click here</a>");
-            }
-            out.println("</center>");
-            out.println("</body>");
-            out.println("</html>");
-        } finally {
-            out.close();
-        }
-
-        /*if(conf_password.equals(password) && !password.isEmpty()) {
-            HttpSession session = request.getSession(true);
             try {
-                UsersDao userDAO = new UsersDao();
-                userDAO.addUserDetails(email, password);
-                response.sendRedirect("Success");
-            } catch (Exception e) {
-
-                e.printStackTrace();
+                UsersDao registerService = new UsersDao();
+                boolean result = registerService.register(user);
+                out.println("<html>");
+                out.println("<head>");
+                out.println("<title>Registration Successful</title>");
+                out.println("</head>");
+                out.println("<body>");
+                out.println("<center>");
+                if (result) {
+                    out.println("<h1>Thanks for Registering with us :</h1>");
+                    out.println("To login with new UserId and Password<a href=login.jsp>Click here</a>");
+                } else {
+                    out.println("<h1>Registration Failed</h1>");
+                    out.println("To try again<a href=register.jsp>Click here</a>");
+                }
+                out.println("</center>");
+                out.println("</body>");
+                out.println("</html>");
+            } finally {
+                out.close();
             }
         }
         else
-            response.sendRedirect("register.jsp");*/
+            response.sendRedirect("register.jsp");
     }
 }
