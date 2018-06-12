f0cd5cf5b8f0a0be998d2fb0002b785e818524a7
pepperoni666 <peretser08@gmail.com>
2018-06-12 02:43:51 +0200

    Last preperetions before netto->brutto calculating algorithm

--- src/calcData/CalcServlet.java
+++ src/calcData/CalcServlet.java
@@ -10,11 +10,48 @@ import java.io.IOException;
 import java.io.PrintWriter;
 
 public class CalcServlet extends HttpServlet {
+
+    private HistoryEntity calcBrutto(String val, String email, double kosztUzyskania){
+        double br = 0, ue = 0, ur = 0, uc = 0, uz = 0, zp = 0, ne = 0;
+
+        try {
+            br = Double.parseDouble(val);
+        } catch (Exception e) {
+            br = Double.valueOf(0);
+        }
+        ue = br * 0.0976;
+        ur = br * 0.015;
+        uc = br * 0.0245;
+        uz = (br - (ue + ur + uc)) * 0.09;
+        double tmp;
+
+        if ((br - (ue + ur + uc + uz) * 12) < 85528)
+            tmp = ((br - (ue + ur + uc)) - kosztUzyskania) * 0.18 - 46.33;
+        else
+            tmp = 15395.04 + ((br - (ue + ur + uc + uz) * 12) - 85528) * 0.32 - 46.33;
+
+        zp = Math.round(tmp - (br - (ue + ur + uc)) * 0.0775);
+
+        ne = br - zp - tmp - uz;
+
+        return new HistoryEntity(email, br, ue, ur, uc, uz, zp, ne);
+    }
+
+    private HistoryEntity calcNetto(String val, String email, double kosztUzyskania){
+        double br = 0, ue = 0, ur = 0, uc = 0, uz = 0, zp = 0, ne = 0;
+
+        try {
+            ne = Double.parseDouble(val);
+        } catch (Exception e) {
+            ne = Double.valueOf(0);
+        }
+
+        return new HistoryEntity(email, br, ue, ur, uc, uz, zp, ne);
+    }
+
     protected void doPost(HttpServletRequest request,
                           HttpServletResponse response) throws ServletException, IOException {
         PrintWriter out = response.getWriter();
-
-        double br = 0, ue = 0, ur = 0, uc = 0, uz = 0, zp = 0, ne = 0;
 
         String val = request.getParameter("wynagrod");
         String gdzie = request.getParameter("gdzie");
@@ -23,37 +60,15 @@ public class CalcServlet extends HttpServlet {
         if(gdzie.equals("on"))
             kosztUzyskania = 111.25;
 
+        UsersEntity user = (UsersEntity) request.getSession().getAttribute("user");
+        HistoryEntity his = null;
         if(tto[0].equals("brutto")) {
-            try {
-                br = Double.parseDouble(val);
-            } catch (Exception e) {
-                br = Double.valueOf(0);
-            }
-            ue = br * 0.0976;
-            ur = br * 0.015;
-            uc = br * 0.0245;
-            uz = (br - (ue + ur + uc)) * 0.09;
-            double tmp;
-
-            if ((br - (ue + ur + uc + uz) * 12) < 85528)
-                tmp = ((br - (ue + ur + uc)) - kosztUzyskania) * 0.18 - 46.33;
-            else
-                tmp = 15395.04 + ((br - (ue + ur + uc + uz) * 12) - 85528) * 0.32 - 46.33;
-
-            zp = Math.round(tmp - (br - (ue + ur + uc)) * 0.0775);
-
-            ne = br - zp - tmp - uz;
+            his = calcBrutto(val, user.getEmail(), kosztUzyskania);
         }
         else{
-            try {
-                ne = Double.parseDouble(val);
-            } catch (Exception e) {
-                ne = Double.valueOf(0);
-            }
+            his = calcNetto(val, user.getEmail(), kosztUzyskania);
         }
         HistoryService service= new HistoryService();
-        UsersEntity user = (UsersEntity) request.getSession().getAttribute("user");
-        HistoryEntity his = new HistoryEntity(user.getEmail(), br, ue, ur, uc, uz, zp, ne);
         service.addToHis(his);
         try {
             out.println("<html>");
@@ -63,13 +78,13 @@ public class CalcServlet extends HttpServlet {
             out.println("<body>");
             out.println("<center>");
             out.println("<h1>Result</h1>");
-            out.println("<br/><b>Brutto: " + br + "</b>");
-            out.println("<p>ubezpieczenie emerytalne: " + ue + "</p>");
-            out.println("<p>ubezpieczenie rentowe: " + ur + "</p>");
-            out.println("<p>ubezpieczenie chorobowe: " + uc + "</p>");
-            out.println("<p>ubezpieczenie zdrowotne: " + uz + "</p>");
-            out.println("<p>zaliczka na PIT: " + zp + "</p>");
-            out.println("<b>Netto: " + ne + "</b>");
+            out.println("<br/><b>Brutto: " + his.getBrutto() + "</b>");
+            out.println("<p>ubezpieczenie emerytalne: " + his.getUbez_emerytalne() + "</p>");
+            out.println("<p>ubezpieczenie rentowe: " + his.getUbez_rentowe() + "</p>");
+            out.println("<p>ubezpieczenie chorobowe: " + his.getUbez_chorobowe() + "</p>");
+            out.println("<p>ubezpieczenie zdrowotne: " + his.getUbez_zdrowotne() + "</p>");
+            out.println("<p>zaliczka na PIT: " + his.getZaliczka_pit() + "</p>");
+            out.println("<b>Netto: " + his.getNetto() + "</b>");
             out.println("<p><a href=home.jsp>Zmien kryteria obliczen</a></p>");
             out.println("</center>");
             out.println("<p><a href=history.jsp>View calculations history</a></p>");
